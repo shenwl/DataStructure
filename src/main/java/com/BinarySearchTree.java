2aeb2afc927e17de184363c86fc66185ed082ae4
shenwl <shenweilun96@gmail.com>
2020-03-14 21:18:04 +0800

    二叉树及其前/中/后序遍历

---
+++ src/main/java/com/BinarySearchTree.java
@@ -0,0 +1,111 @@
+package com;
+
+public class BinarySearchTree<T extends Comparable> {
+    public class Node {
+        T value;
+        Node left, right;
+
+        public Node(T value) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public BinarySearchTree() {
+        this.root = null;
+        this.size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(T e) {
+        root = add(root, e);
+    }
+
+    private Node add(Node node, T e) {
+        if (node == null) {
+            size += 1;
+            return new Node(e);
+        }
+        int compareResult = e.compareTo(node.value);
+        if (compareResult < 0) {
+            node.left = add(node.left, e);
+        } else if (compareResult > 0) {
+            node.right = add(node.right, e);
+        }
+        return node;
+    }
+
+    public boolean contains(T e) {
+        return contains(root, e);
+    }
+
+    private boolean contains(Node node, T e) {
+        int compareResult = e.compareTo(node.value);
+        if (compareResult == 0) {
+            return true;
+        }
+        if (compareResult < 0) {
+            return contains(node.left, e);
+        }
+        return contains(node.right, e);
+    }
+
+    private void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.value);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    /**
+     * 前序遍历
+     */
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.println(node.value);
+        inOrder(node.right);
+    }
+
+    /**
+     * 中序遍历
+     */
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    private void postOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        inOrder(node.right);
+        System.out.println(node.value);
+    }
+
+    /**
+     * 后序遍历
+     */
+    public void postOrder() {
+        postOrder(root);
+    }
+}
---
+++ src/test/java/com/BinarySearchTreeTest.java
@@ -0,0 +1,25 @@
+package com;
+
+public class BinarySearchTreeTest {
+    public static void main(String[] args) {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+
+        //////////////////
+        //      5       //
+        // 3        6   //
+        //2 4         8 //
+        //////////////////
+        bst.add(5);
+        bst.add(3);
+        bst.add(6);
+        bst.add(2);
+        bst.add(4);
+        bst.add(8);
+
+        bst.preOrder();
+        System.out.println("---------");
+        bst.inOrder();
+        System.out.println("---------");
+        bst.postOrder();
+    }
+}
